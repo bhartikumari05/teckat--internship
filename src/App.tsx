9b6979cc50077fc93f5dddb83dedb4958efc9774
bhartikumari <bhartiverma8340@gmail.com>
2024-06-08 18:01:20 +0530

    b

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState<number>();
+  useEffect(() => {
+    setData(414);
+  },[]);
+  return(
+    <>
+    <div>
+      {data}</div>
+      </>
+  );
+
+  }
+
+  export default App;
+  /*const [count, setCount] = useState(0)
 
   return (
     <>
@@ -30,6 +44,6 @@ function App() {
       </p>
     </>
   )
-}
+}*/
 
-export default App
+//export default App
