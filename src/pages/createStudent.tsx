cefb190f111082a2a5fbfe9b09cbe2a7bcb6bb2b
bhartikumari <bhartiverma8340@gmail.com>
2024-06-18 17:19:56 +0530

    bharti18

--- src/pages/createStudent.tsx
+++ src/pages/createStudent.tsx
@@ -1,72 +1,9 @@
 import React from 'react'
-import {userform} form "react-hook-form";
-import {student} form "../interface/student";
-const createStudent : React.FC  = ():any => {
-  const {
-    register,
-    handlesubmit,
-    formstate: {errors},
-  } = useform({mode: "all"});
-  const createNewStudent =(data: IStudent) => {
-    console.log(data);
-  };
-  return(
-   <>
-   <div className="create_student_section">
-        <form onSubmit={handleSubmit(createNewStudent)}>
-          <div>
-            <label htmlFor="name"></label>
-            <input
-              title="name"
-              type="text"
-              id="name"
-              {...register("name", {
-                required: { value: true, message: "Name is required" },
-              })}
-            />
-            <div className="error_message">{errors.name?.message}</div>
-          </div>
-          <div>
-            <label htmlFor="age"></label>
-            <input
-              title="age"
-              type="number"
-              id="age"
-              {...register("age", {
-                required: { value: true, message: "Age is required" },
-              })}
-            />
-            <div className="error_message">{errors.age?.message}</div>
-          </div>
-          <div>
-            <label htmlFor="phone"></label>
-            <input
-              title="phone"
-              type="number"
-              id="phone"
-              {...register("phone", {
-                required: { value: true, message: "Phone is required" },
-              })}
-            />
-            <div className="error_message">{errors.phone?.message}</div>
-          </div>
-          <div>
-            <label htmlFor="email"></label>
-            <input
-              title="email"
-              type="text"
-              id="email"
-              {...register("email", {
-                required: { value: true, message: "Email is required" },
-              })}
-            />
-            <div className="error_message">{errors.email?.message}</div>
-          </div>
 
-          <button title="submit" type="submit">
-            Submit
-          </button>
-        </form>
-      </div>
+const createStudent = () => {
+  return (
+    <div>createStudent</div>
+  )
+}
 
-export default createStudent;
+export default createStudent
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import HomePage from "./Homepage";
+import StudentOutlet from "./studentoutlet";
+import Students from "./student";
+import CreateStudent from "./Createstudent";
+import UpdateStudent from "./updatestudent";
+
+export { HomePage, StudentOutlet, Students, CreateStudent, UpdateStudent};
